5df859b61ab553f831a0da1174e9f48e2fd9b948
joey.f.harrison <joey.f.harrison@8938bf38-a950-6d5f-23da-4050d9021440>
2011-03-24 20:08:04 +0000

    Initial version of the file. Contains very little at this point.

---
+++ trunk/src/retirementAge/RetirementAgeModel.java
@@ -0,0 +1,22 @@
+package retirementAge;
+
+import sim.engine.SimState;
+
+public class RetirementAgeModel extends SimState
+{
+	private static final long serialVersionUID = 1L;
+
+	public RetirementAgeModel(long seed) {
+		super(seed);
+	}
+
+	@Override
+	public void start() {
+		super.start();
+	}
+
+	public static void main(String[] args) {
+		doLoop(RetirementAgeModel.class, args);
+		System.exit(0);
+	}
+}
