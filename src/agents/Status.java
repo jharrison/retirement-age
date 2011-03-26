cd46eb6f32b0cc45a7d253c206ad6c7fe3ba92be
carrknight <carrknight@8938bf38-a950-6d5f-23da-4050d9021440>
2011-03-26 14:49:59 +0000

    This is the ENUM that defines what is happening to the agent. Might even not use it in the end but it's there now

---
+++ src/agents/Status.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package agents;
+
+/**
+ * In this model the agents have three states: Working,Retired,Dead. <br>
+ * I was a bit unsure if I had to use two booleans or an int to define their condition. I am going to try the more natural route of using
+ * a custom enum. <br> Tell me if it is okay
+ * @author carrknight
+ *
+ */
+public enum Status {
+	WORKING, RETIRED, DEAD
+}
