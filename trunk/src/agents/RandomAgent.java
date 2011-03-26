3124b2c641786258dfb937aa00e350338146a2ee
carrknight <carrknight@8938bf38-a950-6d5f-23da-4050d9021440>
2011-03-26 15:56:26 +0000

    here's the random agent

---
+++ trunk/src/agents/RandomAgent.java
@@ -0,0 +1,53 @@
+package agents;
+
+import retirementAge.RetirementAgeModel;
+import ec.util.MersenneTwisterFast;
+
+public class RandomAgent extends Agent {
+
+	/**
+	 * This is the random generator, it should just be linked to the model one so we can control the seed
+	 */
+	private MersenneTwisterFast gen;
+	
+	/**
+	 * On top of the Agent() constructor this one also requires a link to the SimState randomizer to make decisions
+	 * @param currentAge the agent's current age
+	 * @param deathTime the time the agent will die
+	 */
+	public RandomAgent(int currentAge, int deathTime, MersenneTwisterFast random) {
+		//call papa's constructor
+		super(currentAge, deathTime);
+		//link up the random generator
+		gen = random;
+		
+	}
+
+	/**
+	 * annoying
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * This agent, once it is available to him that he can retire, he will throw a coin and decides.
+	 */
+	@Override
+	protected Status doIRetire() {
+
+		//if you are not allowed to retire, don't
+		if(age < RetirementAgeModel.retirementAge)
+			//don't retire
+			return Status.WORKING;
+		//otherwise
+		else{
+			//throw a coin
+			if(gen.nextBoolean())
+				//50% you retire
+				return Status.RETIRED;
+			//50% you don't
+			return Status.WORKING;
+		}
+
+	}
+
+}
