6ee3f55abc841fb53825ef75c5dccdcf3e964098
carrknight <carrknight@8938bf38-a950-6d5f-23da-4050d9021440>
2011-03-26 15:32:31 +0000

    I added a whole bunch of static variables to the model. These are the model parameters that are needed to generate agents

--- src/retirementAge/RetirementAgeModel.java
+++ src/retirementAge/RetirementAgeModel.java
@@ -4,7 +4,47 @@ import sim.engine.SimState;
 
 public class RetirementAgeModel extends SimState
 {
-	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * This is the smallest possible death age for an agent
+	 */
+	public static int minDeathAge = 60;
+	
+	/**
+	 * This is the the minimum age for an agent. This is the first cohort
+	 */
+	public static int minAge=20;
+	
+	/**
+	 * This is the minimum retirement age
+	 */
+	public static int retirementAge = 65;
+	
+	/**
+	 * This is the C of the paper: how many agents for each cohort
+	 */
+	public static int cohortSize = 100;
+	
+	/**
+	 * This is the E of the paper: how many years backwards and forwards should an agent look for when creating his social network?
+	 */
+	public static int networkExtent = 5;
+	
+	/**
+	 * The minimum number of friends in an agent's network
+	 */
+	public static int minNetworkSize = 5;
+	
+	/**
+	 * The maximum number of friends in an agent's network
+	 */
+	public static int maxNetworkSize = 25;
+	
+	
+	/**
+	 * This is the last cohort and also the largest possible death age
+	 */
+	public static int maxAge = 100;
 
 	public RetirementAgeModel(long seed) {
 		super(seed);
